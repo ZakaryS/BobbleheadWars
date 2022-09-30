cf4490abed5ff01c5856dc5934049797080766a0
ZakaryS <zak561@hotmail.com>
2022-09-30 19:39:43 -0400

    Chapter 4 - I caught a cold. :(

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -31,8 +31,11 @@ public class Gun : MonoBehaviour
 
     void fireBullet()
     {
+        //create a new bullet object from the prefab
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        //placing the bullet at the launcher position
         bullet.transform.position = launchPosition.position;
+        //giving the bullet an initial velocity to take off with
         bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
     }
 }
