1f8f8a9be107c8aa642b30316e7047a6bafbfa88
ZakaryS <zak561@hotmail.com>
2022-10-14 21:03:43 -0400

    Chapter 8 - Decapitating the Alien: Everything Broke Here

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -9,8 +9,10 @@ public class Alien : MonoBehaviour
     public Transform target;
     public float navigationUpdate;
     public UnityEvent OnDestroy;
+    public Rigidbody head;
     private NavMeshAgent agent;
     private float navigationTime = 0;
+    public bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,28 +23,42 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Die();
-
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        head.GetComponent<SelfDestruct>().Initiate();
         Destroy(gameObject);
     }
 }
